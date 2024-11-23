1fe3b5ea7643acc36b3fc3ee0c53717053572859
caioolima <caiolimaa2002@gmail.com>
2024-11-23 19:21:16 -0300

    Commit

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 // Lista de origens permitidas (ou '*' para permitir qualquer origem)
-const allowedOrigins = ['http://localhost:3000', 'https://simp-poa.vercel.app/'];
+const allowedOrigins = ['http://localhost:3000', 'https://simp-poa.vercel.app'];
 
 // Configuração do CORS
 const corsOptions = {
