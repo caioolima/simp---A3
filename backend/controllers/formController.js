ebcfe7ddb6f0d10ac75335852ea2268b14f602d5
caioolima <caiolimaa2002@gmail.com>
2024-11-23 22:38:47 -0300

    Commit

--- backend/controllers/formController.js
+++ backend/controllers/formController.js
@@ -1,4 +1,4 @@
-// controllers/FormController.js
+const nodemailer = require('nodemailer');
 const Form = require('../models/formModel');
 
 // Função para listar todos os formulários
@@ -15,6 +15,7 @@ const listarFormularios = async (req, res) => {
   }
 };
 
+// Função para criar um novo formulário
 const criarFormulario = async (req, res) => {
   try {
     const { cep, rua, bairro, cidade, estado, complemento, imagemUrl } = req.body;
@@ -31,13 +32,48 @@ const criarFormulario = async (req, res) => {
       imagemUrl,
     });
 
+    // Salva o novo formulário
     await novoFormulario.save();
+
+    // Enviar um e-mail quando o formulário for criado
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'caioolima1994@gmail.com',
+        pass: 'jmyg ulal stgt nkuc',
+      },
+    });
+
+    const mailOptions = {
+      from: 'caioolima1994@gmail.com',
+      to: 'caiolimaa2002@gmail.com',
+      subject: 'Novo formulário criado',
+      text: `Um novo formulário foi criado com os seguintes dados:\n
+            CEP: ${cep}\n
+            Rua: ${rua}\n
+            Bairro: ${bairro}\n
+            Cidade: ${cidade}\n
+            Estado: ${estado}\n
+            Complemento: ${complemento}\n
+            Imagem URL: ${imagemUrl}\n
+            Status: aguardando verificação`,
+    };
+
+    // Envia o e-mail
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log('Erro ao enviar o e-mail:', error);
+      } else {
+        console.log('E-mail enviado: ' + info.response);
+      }
+    });
+
+    // Responde com o novo formulário
     res.status(201).json(novoFormulario);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Erro ao criar o formulário', error });
   }
 };
-
 
 module.exports = { listarFormularios, criarFormulario };
